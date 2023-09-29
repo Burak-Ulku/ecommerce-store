6cbbba96d6760af65a343222109c67c70877a98b
Burak Ülkü <brkulku@gmail.com>
2023-09-29 16:32:35 +0200

    Create ecommerce store

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 import GenerateButton from './GenerateButton';
+import styles from './page.module.css';
 
 export default function HomePage() {
   return (
-    <main>
-      <GenerateButton />
-
-      <h1>Hello Upleveled</h1>
+    <main className={styles.main}>
+      <header className={styles.heroText}>
+        <div className={styles.div}>
+          <h1>WELCOME TO COoL TOY STORE</h1>
+          <p className="description">
+            This is a store where you can find cool toys.
+          </p>
+          <GenerateButton />
+        </div>
+      </header>
     </main>
   );
 }
