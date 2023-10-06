a79ca2d1103402e07d19df04c3712b1e3855d8a8
Burak Ülkü <brkulku@gmail.com>
2023-10-06 16:45:11 +0200

    Create ecommerce

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import godzilla from '../public/images/godzilla.jpg';
 import GenerateButton from './GenerateButton';
 import styles from './page.module.css';
 
@@ -10,6 +12,14 @@ export default function HomePage() {
           <p className="description">
             This is a store where you can find cool toys.
           </p>
+          <Image
+            src="/images/godzilla.jpg"
+            alt="godzilla"
+            width={750}
+            height={500}
+          />
+          {/* <Image src={godzilla} alt="godzilla" /> */}
+
           <GenerateButton />
         </div>
       </header>
